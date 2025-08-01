9d6ec47d91eca763a3617c167195440fadc3f7b6
deepblue597 <iasonas2001@hotmail.com>
2025-08-01 15:30:40 +0300

    initial push

---
+++ k8s-web-hello/index.mjs
@@ -0,0 +1,15 @@
+import express from "express";
+import os from "os";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const hostname = os.hostname();
+  res.send(`Hello from ${hostname}!`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+// Export the app for testing purposes
