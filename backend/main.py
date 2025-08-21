fe463005db84de8555976c79787b530a35d0311d
deepblue597 <iasonas2001@hotmail.com>
2025-08-21 16:35:22 +0300

    poetry

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 import socket
 from fastapi import FastAPI
-
+import uvicorn
 app = FastAPI()
 
 @app.get("/")
@@ -12,3 +12,6 @@ def read_node():
     hostname = socket.gethostname()
     return {"Node": hostname}
 
+if __name__ == "__main__":
+    print("Starting server on http://127.0.0.1:8009")
+    uvicorn.run(app, host="127.0.0.1", port=8009)
